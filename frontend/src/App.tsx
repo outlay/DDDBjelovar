084574c403cc6d8874e0d4af2a9c2a7d81787b7d
Dario <darioohuic@gmail.com>
2024-10-18 13:10:28 +0200

    Initial frontend app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+    return (
+        <div className="font-bold text-3xl">
+            <Button variant="outline">Button</Button>
+        </div>
+    );
+};
+
+export default App;
