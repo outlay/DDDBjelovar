afe8ae999cf46580a131026e5c7d66a412774d65
marko <marko@SERGEJ>
2024-10-18 17:36:18 +0200

    Added navigation bar

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Header from "./header/Header";
 
 const Layout = () => {
     return (
         <div>
+            <Header></Header>
             <Outlet></Outlet>
         </div>
     );
---
+++ frontend/src/components/layout/header/Header.tsx
@@ -0,0 +1,30 @@
+import { Button } from "@/components/ui/Button";
+import { Link } from "react-router-dom";
+import Logo from "@/assets/logo.svg";
+
+const Header = () => {
+    return (
+        <header className="flex justify-between items-center p-4 bg-white shadow">
+            <Link to="/domovi">
+                <span className="text-lg text-blue-800">Pregled domova</span>
+            </Link>
+            <div className="flex items-center">
+                <Link to="/">
+                    <img className="h-12 w-auto" src={Logo}></img>
+                </Link>
+            </div>
+            <nav className="flex space-x-4 items-center">
+                <Link to="/prijava">
+                    <Button
+                        variant="outline"
+                        className="text-blue-500 border-blue-500 hover:bg-blue-500 hover:text-white"
+                    >
+                        Prijava
+                    </Button>
+                </Link>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
