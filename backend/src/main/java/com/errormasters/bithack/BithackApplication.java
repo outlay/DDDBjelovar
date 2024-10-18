b18746c885f7ffc3152a0fe735bae754e5d55062
mastuhne <matija.alojz.stuhne@5x9networks.com>
2024-10-18 11:39:49 +0200

    Add initial project structure

---
+++ backend/src/main/java/com/errormasters/bithack/BithackApplication.java
@@ -0,0 +1,13 @@
+package com.errormasters.bithack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BithackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BithackApplication.class, args);
+	}
+
+}
