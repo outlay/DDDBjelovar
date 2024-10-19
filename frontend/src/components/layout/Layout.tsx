449f737662960c2fc13faedf74da77bd35cff569
Dario <darioohuic@gmail.com>
2024-10-19 13:33:03 +0200

    auth connect with backend

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./footer/Footer";
 import Header from "./header/Header";
+import { Toaster } from "../ui/toaster";
 
 const Layout = () => {
     return (
         <div>
             <Header></Header>
             <Outlet></Outlet>
+            <Toaster></Toaster>
             <Footer></Footer>
         </div>
     );
