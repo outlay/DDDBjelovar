8cce8f4409b3895b3e782553369d674b02cb94d7
marko <marko@SERGEJ>
2024-10-19 10:59:46 +0200

    Footer added

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom";
+import Footer from "./footer/Footer";
 import Header from "./header/Header";
 
 const Layout = () => {
@@ -6,6 +7,7 @@ const Layout = () => {
         <div>
             <Header></Header>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
